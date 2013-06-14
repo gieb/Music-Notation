750b977d99ab8021f584a723463109da3bad23fc
gieb <pgiebul@gmail.com>
2013-06-14 16:49:11 +0200

    Aktualizacja

---
+++ README.md
@@ -0,0 +1,3 @@
+Po odpaleniu pliku 'compile.sh' powstaje plik 'parser'.
+Uruchamiasz go './parser nazwa_pliku' .
+Plik z przykładowym kodem nazywa się 'sample'
--- compile.sh
+++ compile.sh
@@ -2,4 +2,4 @@
 
 bison -dv parser.y | echo 'bison'
 flex lexer.l | echo 'flex'
-g++ parser.tab.c lex.yy.c -lfl -o parser | 'kompilacja'
+g++ parser.tab.c lex.yy.c -lfl -o parser | echo 'kompilacja'
